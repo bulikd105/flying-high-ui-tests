8e2734af5f135adc83da37cd3dec275940c7ecf0
bulikd105 <bulikd105@gmail.com>
2016-07-05 22:50:24 +0200

    Refactor klasy HomePage

--- src/test/java/edu/iis/mto/bdd/cucumber/pages/HomePage.java
+++ src/test/java/edu/iis/mto/bdd/cucumber/pages/HomePage.java
@@ -1,26 +1,29 @@
 package edu.iis.mto.bdd.cucumber.pages;
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.PageFactory;
 
-import static org.junit.Assert.assertThat;
-import static org.hamcrest.Matchers.equalTo;
-
-import edu.iis.mto.bdd.model.FrequentFlyerMember;
-import net.serenitybdd.core.annotations.findby.FindBy;
+import net.serenitybdd.core.annotations.findby.By;
 import net.serenitybdd.core.pages.PageObject;
 import net.thucydides.core.annotations.DefaultUrl;
 
 @DefaultUrl("http://localhost:8080/#/home")
 public class HomePage extends PageObject
 {
-	@FindBy(id = "welcome-message")
-	private WebElement message;
-	
-	
-	public void checkWelcomeMessage(FrequentFlyerMember user)
-	{
-		assertThat(message.getText(), equalTo("Witaj "+user.getFirstName()));
-	}
+    public HomePage(WebDriver driver) 
+    {
+        super(driver);
+    }
+    
+    public void selectMenuOptions(String... menuOptions) 
+    {
+        for (String menuChoice : menuOptions)
+        {
+            $(".mainMenu").findBy(By.linkText(menuChoice)).click();
+        }
+    }
+
+    public String getWelcomeMessage() 
+    {
+        return getDriver().findElement(By.id("welcome-message")).getText();
+    }
 }
