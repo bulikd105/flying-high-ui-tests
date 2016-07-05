6d05b7b2efea1c901384b4da4853d3172282247b
bulikd105 <bulikd105@gmail.com>
2016-07-05 22:56:09 +0200

    Dodanie kroków w AuthenticationWorkflowSteps

--- src/test/java/edu/iis/mto/bdd/cucumber/workflowsteps/AuthenticationWorkflowSteps.java
+++ src/test/java/edu/iis/mto/bdd/cucumber/workflowsteps/AuthenticationWorkflowSteps.java
@@ -1,5 +1,29 @@
 package edu.iis.mto.bdd.cucumber.workflowsteps;
 
-public class AuthenticationWorkflowSteps {
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
 
+import edu.iis.mto.bdd.cucumber.pages.HomePage;
+import edu.iis.mto.bdd.cucumber.pages.LoginPage;
+import edu.iis.mto.bdd.model.FrequentFlyerMember;
+import net.thucydides.core.annotations.Step;
+
+public class AuthenticationWorkflowSteps 
+{
+	LoginPage loginPage;
+    HomePage homePage;
+
+    @Step
+    public void enterCredentials(FrequentFlyerMember frequentFlyerMember) 
+    {
+        loginPage.open();
+        loginPage.signingWithCredentials(frequentFlyerMember);
+    }
+
+    @Step
+    public void verifyWelcomeMessageFor(FrequentFlyerMember frequentFlyerMember) 
+    {
+        String welcomeMessage = homePage.getWelcomeMessage();
+        assertThat(welcomeMessage, equalTo("Witaj " + frequentFlyerMember.getFirstName()));
+    }
 }
