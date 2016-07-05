18648a9283d6bafbf803e25c55fd6713607f1ea3
bulikd105 <bulikd105@gmail.com>
2016-07-05 22:58:45 +0200

    Refaktor klasy UserAuthenticationSteps, aby korzystała z workflow

--- src/test/java/edu/iis/mto/bdd/cucumber/steps/UserAuthenticationSteps.java
+++ src/test/java/edu/iis/mto/bdd/cucumber/steps/UserAuthenticationSteps.java
@@ -1,29 +1,22 @@
 package edu.iis.mto.bdd.cucumber.steps;
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
-
 import cucumber.api.java.After;
-import cucumber.api.java.Before;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
-import edu.iis.mto.bdd.cucumber.pages.HomePage;
-import edu.iis.mto.bdd.cucumber.pages.LoginPage;
+import edu.iis.mto.bdd.cucumber.workflowsteps.AuthenticationWorkflowSteps;
 import edu.iis.mto.bdd.model.FrequentFlyerMember;
+import net.thucydides.core.annotations.Managed;
+import net.thucydides.core.annotations.Steps;
 
 public class UserAuthenticationSteps 
 {
-	private WebDriver driver ;
-	private LoginPage loginPage;
-	private HomePage homePage;
-	
-	@Before
-	public void init()
-	{
-		driver = new FirefoxDriver();
-	}
-	
+    @Steps
+    AuthenticationWorkflowSteps authenticationWorkflowSteps;
+
+    @Managed
+    private WebDriver driver;
 	
     @Given("^(.*) is a registered Frequent Flyer$")
     public void givenARegisteredFrequentFlyer(FrequentFlyerMember user) {}
@@ -31,24 +24,19 @@ public class UserAuthenticationSteps
     @When("^(.*) authenticates with a valid email address and password$")
     public void whenJaneAuthenticatesWithAValidEmailAddressAndPassword(FrequentFlyerMember user) 
     {
-    	loginPage.open();
-    	loginPage.signingWithCredentials(user);
-
+    	authenticationWorkflowSteps.enterCredentials(user);
     }
 
     @Then("^(.*) should be given access to (?:her|his) account$")
     public void thenTheUserShouldBeGivenAccessToAccount(FrequentFlyerMember user) 
     {
-    
-    	homePage.open();
-    	homePage.getWelcomeMessage();
+    	authenticationWorkflowSteps.verifyWelcomeMessageFor(user);
     }
 
     @Given("^(.*) has logged on$")
     public void aUserHasLoggedOnAs(FrequentFlyerMember user) 
     {
-		loginPage.open();
-    	loginPage.signingWithCredentials(user);
+    	authenticationWorkflowSteps.enterCredentials(user);
     }
 
     @When("^(?:.*) views the home page$")
